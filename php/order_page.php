d77b59b3835037a50e5a021adad8586197700d55
StevenSutantoh <steven.sutantoh@gmail.com>
2022-03-31 00:32:23 +0700

    refresh

--- php/order_page.php
+++ php/order_page.php
@@ -1,3 +1,9 @@
 <?php
     require __DIR__ . '/../connect_database.php';
+    function queryCart($user_id,$link){
+        $sql = "
+
+        ";
+        $result = mysqli_query($link,$sql);
+        return $result;
 ?>
