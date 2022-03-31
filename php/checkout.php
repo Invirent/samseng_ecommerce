7009caee18c04bf59f30195876a683dc935bd1ae
Vega-29 <vincent.ega49@gmail.com>
2022-04-01 00:11:26 +0700

    checkout

--- php/checkout.php
+++ php/checkout.php
@@ -1,4 +1,14 @@
 <?php
     require __DIR__ . '/../connect_database.php';
+    $conn = mysqli_connect('localhost','20si2','uphmedan');
     include '../html/checkout.html';
+    $Alamat = $_POST['address'];
+    
+    // var_dump($_POST);die();
+    
+    $query = "INSERT INTO sale_order (address) values (?)";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s",$Alamat);
+
+    
 ?>
