f87077f83e4c167a5daa2324c0af2c0d6f98c4a5
StevenSutantoh <steven.sutantoh@gmail.com>
2022-04-01 01:28:11 +0700

    new

--- index.php
+++ index.php
@@ -40,9 +40,6 @@
                     <li class="nav-item">
                         <a class="nav-link" href="php/shopping_cart_template.php"><i class="fa fa-shopping-cart"></i>Cart</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="php/checkout.php"><i class="fa fa-checkout"></i>Checkout</a>
-                    </li>
                 </ul>
             </div>
             <?php
