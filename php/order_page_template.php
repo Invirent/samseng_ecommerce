047040bd36e76098f94de2b963710db4b852dac2
StevenSutantoh <steven.sutantoh@gmail.com>
2022-04-18 23:28:46 +0700

    menambahkan carousel di order page

--- php/order_page_template.php
+++ php/order_page_template.php
@@ -58,15 +58,10 @@
             padding-bottom: 50px;
         }
 
-        /* .product-detail {
-            float: right;
-        } */
-
-        /* .card-img-top {
-            height: 30%;
-            width: 30%;
-        } */
-
+        .carousel {
+            max-width: 100%;
+        }
+        
 .show-btn{
   background: #fff;
   padding: 10px 20px;
@@ -310,7 +305,35 @@ form .btn button{
     <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script> 
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <script src="..//jquery.mycart.js"></script>
-
+    <div>
+        <div id='carouselExampleIndicators' class='carousel slide' data-bs-ride='carousel'>
+            <div class='carousel-indicators'>
+                <button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='0' class='active' aria-current='true' aria-label='Slide 1'></button>
+                <button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='1' aria-label='Slide 2'></button>
+                <button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='2' aria-label='Slide 3'></button>
+            </div>
+            <div class="carousel-inner">
+                        <div class='carousel-item active'>
+                            <img src='../static/img/carousel-ramadhan.png' class='d-block w-100' alt='...'>
+                        </div>
+                        <div class='carousel-item'>
+                            <img src='../static/img/carousel-ramadhan3.png' class='d-block w-100' alt='...'>
+                        </div>
+                        <div class='carousel-item'>
+                            <img src='../static/img/carousel-ramadhan2.png' class='d-block w-100' alt='...'>
+                        </div>
+                    </div>
+                    <button class='carousel-control-prev' type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide='prev'>
+                        <span class='carousel-control-prev-icon' aria-hidden='true'></span>
+                        <span class='visually-hidden'>Previous</span>
+                    </button>
+                    <button class='carousel-control-next' type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide='next'>
+                        <span class='carousel-control-next-icon' aria-hidden='true'></span>
+                        <span class='visually-hidden'>Next</span>
+                    </button>
+                </div>
+					</div>
+    </div>
     <footer>
         <div class="container-fluid">
             <div class="row">
